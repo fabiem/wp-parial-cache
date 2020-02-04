71514399c766ceef66d1253225933be27af9bfb0
Fabien MOTTA <mottafabien1@macbook-pro-de-fabien.home>
2020-02-04 15:05:18 +0100

    Version 1.0 avec les options de clean cache

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$GLOBALS['wppcachepurge']['id'] = array(
+  // 2 => array( array(url, zone) )
+
+);
+
+$GLOBALS['wppcachepurge']['type'] = array(
+  // 'post' => array( array(url, zone) )
+  
+
+);
